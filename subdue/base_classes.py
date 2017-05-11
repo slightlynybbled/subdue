2b022b1b652c56652399d907cd60a8a21abfdc05
Jason R. Jones <slightlynybbled@gmail.com>
2017-05-11 07:38:21 -0400

    Added docstring

--- subdue/base_classes.py
+++ subdue/base_classes.py
@@ -3,4 +3,11 @@
 class HardwareSearch:
 
     def list_devices(self):
+        """
+        This function should return a list of suitable references with which the device can 
+        be initialized.  For instance, Visa Instruments may require model numbers while DAQmx
+        devices may require OS references such as 'Dev1'.
+        
+        :return: list containing all detected hardware of this class
+        """
         raise NotImplementedError
